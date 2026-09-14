5b67086c9ec0a1b3d60494d4d392661705d4d7ac
chris <chri.perezv@duocuc.cl>
2026-09-14 00:20:35 -0300

    Implementacion Swagger

--- src/main/java/com/digitalfix/msusuarios/service/AppUserService.java
+++ src/main/java/com/digitalfix/msusuarios/service/AppUserService.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,7 +24,7 @@ public class AppUserService {
 
     @Transactional
     public AppUser autoProvision(String azureOid, String email, String name, String role) {
-        Optional< AppUser > existingUser = appUserRepository.findByAzureOid(azureOid);
+        Optional < AppUser > existingUser = appUserRepository.findByAzureOid(azureOid);
 
         if (existingUser.isPresent()) {
             AppUser user = existingUser.get();
@@ -44,16 +45,18 @@ public class AppUserService {
         newUser.setEmail(email);
         newUser.setName(name);
         newUser.setCompany(defaultCompany);
-        newUser.setRole(role); // <-- Asignamos el rol proveniente de Azure
-        newUser.setActive(true); // <-- Nos aseguramos de que inicie activo
+        newUser.setRole(role);
+        newUser.setActive(true);
 
         try {
-            // saveAndFlush fuerza la escritura inmediata para gatillar el error de UNIQUE si hay colisión
             return appUserRepository.saveAndFlush(newUser);
         } catch (DataIntegrityViolationException e) {
-            // Si otro hilo lo creó fracciones de segundo antes, lo recuperamos
             return appUserRepository.findByAzureOid(azureOid).orElseThrow();
         }
+    }
+
+    public List < AppUser > getAllUsers() {
+        return appUserRepository.findAll();
     }
 
     public AppUser updateStatus(String oid, boolean active) {
@@ -61,5 +64,13 @@ public class AppUserService {
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado con OID: " + oid));
         user.setActive(active);
         return appUserRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteUser(String oid) {
+        AppUser user = appUserRepository.findByAzureOid(oid)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado con OID para eliminar: " + oid));
+
+        appUserRepository.delete(user);
     }
 }
